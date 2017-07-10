9fd6d193e81a38c8dff535fb6c653aff39622a03
Peter Parada <parad.peter@gmail.com>
2017-07-10 22:15:53 +0100

    Prepare interface for storage handler

---
+++ index.js
@@ -0,0 +1,18 @@
+// TODO: implement
+function GoogleCloudStorage (opts) {
+
+}
+
+// TODO: implement
+GoogleCloudStorage.prototype._handleFile = function (req, file, cb) {
+
+}
+
+// TODO: implement
+GoogleCloudStorage.prototype._removeFile = function (req, file, cb) {
+
+}
+
+module.exports = function (opts) {
+  return new GoogleCloudStorage(opts)
+}
