0d5048131da5923058877ab31584b008c44c33ed
Peter Parada <parad.peter@gmail.com>
2017-07-11 21:44:08 +0200

    Add check for bucket

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // TODO: implement
 function GoogleCloudStorage (opts) {
+  switch (typeof opts.bucket) {
+    case 'function': this.bucket = opts.bucket; break
+    case 'string': this.bucket = staticValue(opts.bucket); break
+    case 'undefined': throw new Error('Bucket is required')
+    default: throw new TypeError('Expected opts.bucket to be undefined, string or function')
+  }
+}
 
+function staticValue (value) {
+  return function (req, file, cb) {
+    cb(null, value)
+  }
 }
 
 // TODO: implement
